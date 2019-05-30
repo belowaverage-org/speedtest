4ee352af5b3a266c8ea654328c04050a3762c504
Dylan Bickerstaff <dylan@belowaverage.org>
2019-05-29 22:22:06 -0400

    Customized for belowaverage.org & IIS

--- speedtest_worker.js
+++ speedtest_worker.js
@@ -43,23 +43,23 @@ var settings = {
 	time_auto: true, // if set to true, tests will take less time on faster connections
 	time_ulGraceTime: 3, //time to wait in seconds before actually measuring ul speed (wait for buffers to fill)
 	time_dlGraceTime: 1.5, //time to wait in seconds before actually measuring dl speed (wait for TCP window to increase)
-	count_ping: 10, // number of pings to perform in ping test
+	count_ping: 50, // number of pings to perform in ping test
 	url_dl: "garbage.php", // path to a large file or garbage.php, used for download test. must be relative to this js file
-	url_ul: "empty.php", // path to an empty file, used for upload test. must be relative to this js file
-	url_ping: "empty.php", // path to an empty file, used for ping test. must be relative to this js file
+	url_ul: "null", // path to an empty file, used for upload test. must be relative to this js file
+	url_ping: "null", // path to an empty file, used for ping test. must be relative to this js file
 	url_getIp: "getIP.php", // path to getIP.php relative to this js file, or a similar thing that outputs the client's ip
-	getIp_ispInfo: true, //if set to true, the server will include ISP info with the IP address
-	getIp_ispInfo_distance: "km", //km or mi=estimate distance from server in km/mi; set to false to disable distance estimation. getIp_ispInfo must be enabled in order for this to work
-	xhr_dlMultistream: 6, // number of download streams to use (can be different if enable_quirks is active)
-	xhr_ulMultistream: 3, // number of upload streams to use (can be different if enable_quirks is active)
-	xhr_multistreamDelay: 300, //how much concurrent requests should be delayed
+	getIp_ispInfo: false, //if set to true, the server will include ISP info with the IP address
+	getIp_ispInfo_distance: "mi", //km or mi=estimate distance from server in km/mi; set to false to disable distance estimation. getIp_ispInfo must be enabled in order for this to work
+	xhr_dlMultistream: 5, // number of download streams to use (can be different if enable_quirks is active)
+	xhr_ulMultistream: 5, // number of upload streams to use (can be different if enable_quirks is active)
+	xhr_multistreamDelay: 0, //how much concurrent requests should be delayed
 	xhr_ignoreErrors: 1, // 0=fail on errors, 1=attempt to restart a stream if it fails, 2=ignore all errors
 	xhr_dlUseBlob: false, // if set to true, it reduces ram usage but uses the hard drive (useful with large garbagePhp_chunkSize and/or high xhr_dlMultistream)
 	xhr_ul_blob_megabytes: 20, //size in megabytes of the upload blobs sent in the upload test (forced to 4 on chrome mobile)
-	garbagePhp_chunkSize: 100, // size of chunks sent by garbage.php (can be different if enable_quirks is active)
+	garbagePhp_chunkSize: 200, // size of chunks sent by garbage.php (can be different if enable_quirks is active)
 	enable_quirks: true, // enable quirks for specific browsers. currently it overrides settings to optimize for specific browsers, unless they are already being overridden with the start command
 	ping_allowPerformanceApi: true, // if enabled, the ping test will attempt to calculate the ping more precisely using the Performance API. Currently works perfectly in Chrome, badly in Edge, and not at all in Firefox. If Performance API is not supported or the result is obviously wrong, a fallback is provided.
-	overheadCompensationFactor: 1.06, //can be changed to compensatie for transport overhead. (see doc.md for some other values)
+	overheadCompensationFactor: 1, //can be changed to compensatie for transport overhead. (see doc.md for some other values)
 	useMebibits: false, //if set to true, speed will be reported in mebibits/s instead of megabits/s
 	telemetry_level: 0, // 0=disabled, 1=basic (results only), 2=full (results and timing) 3=debug (results+log)
 	url_telemetry: "telemetry/telemetry.php", // path to the script that adds telemetry data to the database
@@ -254,6 +254,9 @@ this.addEventListener("message", function(e) {
 		ulStatus = "";
 		pingStatus = "";
 		jitterStatus = ""; // set test as aborted
+	}
+	if (params[0] === "getip") {
+		getIp(function(){});
 	}
 });
 // stops all XHR activity, aggressively
